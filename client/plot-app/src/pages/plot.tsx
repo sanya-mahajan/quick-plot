4b6076d0be297e06c37044c65919fd28cfbfc17b
sanya mahajan <sanyamahajan08@gmail.com>
2023-08-26 19:31:07 +0530

    create coordinates fields

--- client/plot-app/src/pages/plot.tsx
+++ client/plot-app/src/pages/plot.tsx
@@ -15,7 +15,8 @@ export default function Plot(): JSX.Element {
     datasets: [],
   });
   const [tableData, setTableData] = useState<DataEntry[]>([]);
-
+  const [newX, setNewX] = useState<number>(0);
+  const [newY, setNewY] = useState<number>(0);
   const chartRef = useRef<Chart | null>(null);
 
   useEffect(() => {
@@ -31,7 +32,7 @@ export default function Plot(): JSX.Element {
           labels: xCoordinates,
           datasets: [
             {
-              label: new Date().getFullYear().toString(),
+              label: "Label",
               backgroundColor: "#3182ce",
               borderColor: "#3182ce",
               data: yCoordinates,
@@ -64,6 +65,29 @@ export default function Plot(): JSX.Element {
     }
   }, [chartData]);
 
+  const handleAddData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/plots/new/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ x: newX, y: newY }),
+      });
+
+      if (response.ok) {
+        const newData: DataEntry = await response.json();
+        setTableData([...tableData, newData]);
+        setNewX(0);
+        setNewY(0);
+      } else {
+        console.error("Error adding data");
+      }
+    } catch (error) {
+      console.error("Error adding data:", error);
+    }
+  };
+
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
@@ -71,7 +95,7 @@ export default function Plot(): JSX.Element {
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">Overview</h6>
-              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+              
             </div>
           </div>
         </div>
@@ -104,6 +128,29 @@ export default function Plot(): JSX.Element {
                 ))}
               </tbody>
             </table>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="text-white font-semibold mb-2">Add Data</h3>
+            <div className="flex space-x-4">
+              <input
+                type="number"
+                placeholder="X Value"
+                value={newX}
+                onChange={(e) => setNewX(parseFloat(e.target.value))}
+                className="border p-2"
+              />
+              <input
+                type="number"
+                placeholder="Y Value"
+                value={newY}
+                onChange={(e) => setNewY(parseFloat(e.target.value))}
+                className="border p-2"
+              />
+              <button onClick={handleAddData} className="bg-blue-500 text-blue px-4 py-2">
+                Add
+              </button>
+            </div>
           </div>
       
    
